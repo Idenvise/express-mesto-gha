0719f0afaf83a530135434ae1c729010372a5f52
Pavel Zavezenov <pavel290430@yandex.ru>
2022-09-04 14:14:21 +0700

    ПР14

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.post('/signup', celebrate({
     about: Joi.string().min(2).max(30),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
+    // eslint-disable-next-line no-useless-escape
     avatar: Joi.string().regex(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/),
   }),
 }), postUser);
--- routes/cards.js
+++ routes/cards.js
@@ -12,17 +12,19 @@ router.post('/cards', celebrate({
     link: Joi.string().required().regex(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/),
   }),
 }), postCard);
-router.delete('/cards/:_id', deleteCard);
+router.delete('/cards/:_id', celebrate({
+  body: Joi.object().keys({
+    _id: Joi.string().min(24),
+  }),
+}), deleteCard);
 router.put('/cards/:_id/likes', celebrate({
   body: Joi.object().keys({
-    // eslint-disable-next-line no-useless-escape
-    _id: Joi.string().regex(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/),
+    _id: Joi.string().min(24),
   }),
 }), setLike);
 router.delete('/cards/:_id/likes', celebrate({
   body: Joi.object().keys({
-    // eslint-disable-next-line no-useless-escape
-    _id: Joi.string().regex(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/),
+    _id: Joi.string().min(24),
   }),
 }), removeLike);
 

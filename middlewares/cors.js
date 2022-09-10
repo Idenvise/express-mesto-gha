3c349ac036dea3a5e10f3525140736694a971a3d
Pavel Zavezenov <pavel290430@yandex.ru>
2022-09-10 19:23:45 +0700

    ПР15

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,5 +1,19 @@
 const cors = require('cors');
 
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+];
+
+function setPermittedSources(req, res, next) {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  next();
+}
+
 const options = {
   origin: [
     'http://localhost:порт',
@@ -16,4 +30,5 @@ const options = {
 module.exports = {
   cors,
   options,
+  setPermittedSources,
 };

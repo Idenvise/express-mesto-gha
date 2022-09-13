f689e9520845a1f2b5a96b15bba7730e22ddc5c7
Pavel Zavezenov <pavel290430@yandex.ru>
2022-09-13 23:24:45 +0700

    a

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+
 const { errors, Joi, celebrate } = require('celebrate');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
@@ -14,7 +14,7 @@ const NotFoundError = require('./errors/notFoundError');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(cors());
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));

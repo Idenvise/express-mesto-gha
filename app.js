e054996f591edbea5055787c4b5a40a65115ef3e
Pavel Zavezenov <pavel290430@yandex.ru>
2022-09-13 23:16:40 +0700

    a

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ const NotFoundError = require('./errors/notFoundError');
 const options = {
   origin: '*',
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-  credentials: true,
 };
 const { PORT = 3000 } = process.env;
 const app = express();

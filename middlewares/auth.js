2fb74bef2786d14a5694632b7a0bdcbe869232e9
Pavel Zavezenov <pavel290430@yandex.ru>
2022-09-17 22:11:51 +0700

    a

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,13 +3,13 @@ const { ERROR_UNAUTHORIZED } = require('../errors/errors');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { cookie } = req.headers;
-  if (!cookie) {
+  const { token } = req.headers.authorization;
+  if (!token) {
     return res.status(ERROR_UNAUTHORIZED).send({ message: 'Необходима авторизация' });
   }
   let payload;
   try {
-    payload = jwt.verify(cookie.replace('token=', ''), 'super-giga-mega-secret-key');
+    payload = jwt.verify(token, 'super-giga-mega-secret-key');
   } catch (err) {
     return res
       .status(ERROR_UNAUTHORIZED)

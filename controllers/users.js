ea6fc015a55c15a2b60b0d75694474ca3ea0f10a
Pavel Zavezenov <pavel290430@yandex.ru>
2022-09-18 00:32:47 +0700

    a

--- controllers/users.js
+++ controllers/users.js
@@ -74,9 +74,7 @@ module.exports.postUser = (req, res, next) => {
 module.exports.patchUser = (req, res, next) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about }, { runValidators: true, new: true })
-    .then((user) => {
-      console.log(user)
-      res.send(user); })
+    .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new ValidationError('Некорректные данные'));
@@ -89,7 +87,7 @@ module.exports.patchUser = (req, res, next) => {
 module.exports.patchUserAvatar = (req, res, next) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, { runValidators: true, new: true })
-    .then((user) => res.send({ avatar: user.avatar }))
+    .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new ValidationError('Некорректные данные'));
